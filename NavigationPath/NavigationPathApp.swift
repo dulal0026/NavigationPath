ba5013eccd8ad1e2f8825ea8a1efcd6771c27c2e
dulalDP <115469610+dulalDP@users.noreply.github.com>
2025-09-21 12:28:01 +0600

    Initial Commit

---
+++ NavigationPath/NavigationPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPathApp.swift
+//  NavigationPath
+//
+//  Created by Dulal Hossain on 21/9/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
