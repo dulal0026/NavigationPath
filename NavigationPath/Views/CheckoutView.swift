95fca5c65a79ef1c832db9b2a8040a5fbbdc5d98
dulalDP <115469610+dulalDP@users.noreply.github.com>
2025-09-21 13:36:36 +0600

    Navigate and pop from differennt views

---
+++ NavigationPath/Views/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  NavigationPath
+//
+//  Created by Dulal Hossain on 21/9/25.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView()
+}
---
+++ NavigationPath/Views/ProductDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailsView.swift
+//  NavigationPath
+//
+//  Created by Dulal Hossain on 21/9/25.
+//
+
+import SwiftUI
+
+struct ProductDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailsView()
+}
